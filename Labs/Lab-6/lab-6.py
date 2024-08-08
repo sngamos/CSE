f9a1c54387d072bbf5ae2f250bcadc6216c26877
Codemos <sngamos9@gmail.com>
2024-08-08 12:27:42 +0800

    completed lab 9

---
+++ Labs/Lab-6/lab-6.py
@@ -0,0 +1,176 @@
+#packet size = 56
+#csail.mit.edu
+'''
+PING csail.mit.edu (128.52.132.19) 56(84) bytes of data.
+64 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=1 ttl=30 time=236 ms
+64 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=2 ttl=30 time=235 ms
+64 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=3 ttl=30 time=235 ms
+64 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=4 ttl=33 time=229 ms
+64 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=5 ttl=33 time=230 ms
+64 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=6 ttl=30 time=233 ms
+64 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=7 ttl=30 time=237 ms
+64 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=8 ttl=33 time=230 ms
+64 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=9 ttl=30 time=235 ms
+64 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=10 ttl=30 time=237 ms
+
+--- csail.mit.edu ping statistics ---
+10 packets transmitted, 10 received, 0% packet loss, time 45048ms
+rtt min/avg/max/mdev = 229.216/233.685/237.084/2.796 ms
+'''
+#berkeley.edu
+'''
+PING berkley.edu (185.53.177.71) 56(84) bytes of data.
+64 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=1 ttl=47 time=275 ms
+64 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=2 ttl=50 time=185 ms
+64 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=3 ttl=47 time=277 ms
+64 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=4 ttl=50 time=183 ms
+64 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=5 ttl=47 time=275 ms
+64 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=6 ttl=50 time=186 ms
+64 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=7 ttl=47 time=270 ms
+64 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=8 ttl=50 time=184 ms
+64 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=9 ttl=50 time=183 ms
+64 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=10 ttl=47 time=272 ms
+
+--- berkley.edu ping statistics ---
+10 packets transmitted, 10 received, 0% packet loss, time 45042ms
+rtt min/avg/max/mdev = 182.886/228.995/277.055/44.986 ms
+'''
+
+#usyd.edu.au
+'''
+PING usyd.edu.au (20.248.131.216) 56(84) bytes of data.
+64 bytes from 20.248.131.216 (20.248.131.216): icmp_seq=1 ttl=109 time=111 ms
+64 bytes from 20.248.131.216 (20.248.131.216): icmp_seq=2 ttl=107 time=166 ms
+64 bytes from 20.248.131.216 (20.248.131.216): icmp_seq=3 ttl=107 time=270 ms
+64 bytes from 20.248.131.216 (20.248.131.216): icmp_seq=4 ttl=107 time=116 ms
+64 bytes from 20.248.131.216 (20.248.131.216): icmp_seq=5 ttl=107 time=123 ms
+64 bytes from 20.248.131.216 (20.248.131.216): icmp_seq=6 ttl=107 time=102 ms
+64 bytes from 20.248.131.216 (20.248.131.216): icmp_seq=7 ttl=107 time=102 ms
+64 bytes from 20.248.131.216 (20.248.131.216): icmp_seq=8 ttl=107 time=99.7 ms
+64 bytes from 20.248.131.216 (20.248.131.216): icmp_seq=9 ttl=107 time=104 ms
+64 bytes from 20.248.131.216 (20.248.131.216): icmp_seq=10 ttl=107 time=105 ms
+
+--- usyd.edu.au ping statistics ---
+10 packets transmitted, 10 received, 0% packet loss, time 45032ms
+rtt min/avg/max/mdev = 99.700/129.775/269.610/50.162 ms
+'''
+#kyoto-u.ac.jp
+'''
+PING kyoto-u.ac.jp (151.101.2.132) 56(84) bytes of data.
+64 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=1 ttl=56 time=6.06 ms
+64 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=2 ttl=56 time=7.94 ms
+64 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=3 ttl=56 time=8.39 ms
+64 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=4 ttl=56 time=7.60 ms
+64 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=5 ttl=56 time=7.53 ms
+64 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=6 ttl=56 time=7.04 ms
+64 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=7 ttl=56 time=4.95 ms
+64 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=8 ttl=56 time=4.47 ms
+64 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=9 ttl=56 time=7.77 ms
+64 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=10 ttl=56 time=7.17 ms
+
+--- kyoto-u.ac.jp ping statistics ---
+10 packets transmitted, 10 received, 0% packet loss, time 45041ms
+rtt min/avg/max/mdev = 4.465/6.890/8.388/1.243 ms
+'''
+
+dict_56 = {"www.cail.mit.edu.sg":{"successful":100,"min RTT":229.216,"ave RTT":233.685,"max RTT":237.084},
+        "www.berkeley.edu":{"successful":100,"min RTT":182.886,"ave RTT":228.995,"max RTT":277.055},
+        "www.usyd.edu.au":{"successful":100,"min RTT":99.700,"ave RTT":129.775,"max RTT":269.610},
+        "www.kyoto-u.ac.jp":{"successful":100,"min RTT":4.465,"ave RTT":6.890,"max RTT":8.388},
+        }
+
+
+
+#packet size = 512
+#csail.mit.edu
+'''
+PING csail.mit.edu (128.52.132.19) 512(540) bytes of data.
+520 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=1 ttl=33 time=231 ms
+520 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=2 ttl=30 time=302 ms
+520 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=3 ttl=33 time=230 ms
+520 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=4 ttl=33 time=231 ms
+520 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=5 ttl=33 time=230 ms
+520 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=6 ttl=33 time=231 ms
+520 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=7 ttl=30 time=237 ms
+520 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=8 ttl=33 time=231 ms
+520 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=9 ttl=30 time=244 ms
+520 bytes from inquir-3ld-2022.csail.mit.edu (128.52.132.19): icmp_seq=10 ttl=30 time=235 ms
+
+--- csail.mit.edu ping statistics ---
+10 packets transmitted, 10 received, 0% packet loss, time 45049ms
+rtt min/avg/max/mdev = 229.999/240.200/301.769/20.934 ms
+'''
+#berkeley.edu
+'''
+PING berkley.edu (185.53.177.71) 512(540) bytes of data.
+520 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=1 ttl=47 time=265 ms
+520 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=2 ttl=50 time=186 ms
+520 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=3 ttl=47 time=267 ms
+520 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=4 ttl=50 time=184 ms
+520 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=5 ttl=47 time=265 ms
+520 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=6 ttl=50 time=186 ms
+520 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=7 ttl=50 time=185 ms
+520 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=8 ttl=47 time=256 ms
+520 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=9 ttl=47 time=266 ms
+520 bytes from 185.53.177.71 (185.53.177.71): icmp_seq=10 ttl=50 time=186 ms
+
+--- berkley.edu ping statistics ---
+10 packets transmitted, 10 received, 0% packet loss, time 45041ms
+rtt min/avg/max/mdev = 184.414/224.641/267.389/39.259 ms
+'''
+#usyd.edu.au
+'''
+PING usyd.edu.au (20.248.131.216) 512(540) bytes of data.
+
+--- usyd.edu.au ping statistics ---
+10 packets transmitted, 0 received, 100% packet loss, time 45399ms
+'''
+#kyoto-u.ac.jp
+'''
+PING kyoto-u.ac.jp (151.101.2.132) 512(540) bytes of data.
+520 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=1 ttl=56 time=7.30 ms
+520 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=2 ttl=56 time=7.22 ms
+520 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=3 ttl=56 time=6.76 ms
+520 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=4 ttl=56 time=7.53 ms
+520 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=5 ttl=56 time=9.43 ms
+520 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=6 ttl=56 time=9.09 ms
+520 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=7 ttl=56 time=6.71 ms
+520 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=8 ttl=56 time=8.21 ms
+520 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=9 ttl=56 time=4.83 ms
+520 bytes from 151.101.2.132 (151.101.2.132): icmp_seq=10 ttl=56 time=6.80 ms
+
+--- kyoto-u.ac.jp ping statistics ---
+10 packets transmitted, 10 received, 0% packet loss, time 45042ms
+rtt min/avg/max/mdev = 4.829/7.386/9.429/1.246 ms
+'''
+
+dict_512 = {"www.cail.mit.edu.sg":{"successful":100,"min RTT":229.999,"ave RTT":240.200,"max RTT":301.769},
+        "www.berkeley.edu":{"successful":100,"min RTT":184.414,"ave RTT":224.641,"max RTT":267.389},
+        "www.usyd.edu.au":{"successful":0,"min RTT":0,"ave RTT":0,"max RTT":0},
+        "www.kyoto-u.ac.jp":{"successful":100,"min RTT":4.829,"ave RTT":7.386,"max RTT":9.429},
+        }
+
+#packet size = 1024
+#csail.mit.edu
+'''
+'''
+#berkeley.edu
+'''
+'''
+#usyd.edu.au
+'''
+'''
+#kyoto-u.ac.jp
+'''
+10 packets transmitted, 10 received, 0% packet loss, time 45042ms
+rtt min/avg/max/mdev = 4.829/7.386/9.429/1.246 ms
+sngamos@Amos-RB15:/mnt/c/Users/sngam$ ping -c 10 -s 1024 -i 5 kyoto-u.ac.jp
+'''
+
+dict_1024 = {"www.cail.mit.edu.sg":{"successful":0,"min RTT":0,"ave RTT":0,"max RTT":0},
+        "www.berkeley.edu":{"successful":0,"min RTT":0,"ave RTT":0,"max RTT":0},
+        "www.usyd.edu.au":{"successful":0,"min RTT":0,"ave RTT":0,"max RTT":0},
+        "www.kyoto-u.ac.jp":{"successful":0,"min RTT":0,"ave RTT":0,"max RTT":0},
+        }
+
+
